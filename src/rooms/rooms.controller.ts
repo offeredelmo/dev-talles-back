193d1654012798cc4f1d448dcb50d4e36171f36a
211099 <211099@ids.upchiapas.edu.mx>
2024-03-18 01:22:16 -0600

    se uso el auth para que los enpoints que son para admins solo puedan ser usado por un token que contenga el rol admin

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 import { CreateAwardDto } from './dto/create-award.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/common/rol.enum';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { RoleGuard } from 'src/auth/guard/role.guard';
+import { Roles } from 'src/auth/decorators/roles.decorators';
 
 
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Post()
   create(@Body() createRoomDto: CreateRoomDto) {
     return this.roomsService.create(createRoomDto);
@@ -23,6 +30,8 @@ export class RoomsController {
     return this.roomsService.addUserToRoom(id_room, id_user)
   }
 
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Post("/addaward/:id")
   addAwardToRoom(
     @Param('id', ParseUUIDPipe) id:string,
@@ -30,7 +39,9 @@ export class RoomsController {
   ){
     return this.roomsService.addAwardToRoom(id,createAwardDto)
   }
-  
+
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Get()
   findAll() {
     return this.roomsService.findAllRooms();
@@ -41,16 +52,22 @@ export class RoomsController {
     return this.roomsService.findVisibleRooms();
   }
 
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Put(':id')
   update(@Param('id',ParseUUIDPipe) id:string, @Body() updateRoomDto:UpdateRoomDto){
     return this.roomsService.update(id, updateRoomDto)
   }
 
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Delete(':id')
   delete(@Param('id', ParseUUIDPipe) id:string){
     return this.roomsService.remove(id)
   }
 
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Delete('/award/:id')
   deleteAwardToRoomById(@Param('id', ParseUUIDPipe) id:string){
     return this.roomsService.deleteAwardToRoomById(id)
